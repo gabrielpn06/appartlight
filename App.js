4e3a6928339a82b4d588a8184f14d6cdcd9292b6
Gabriel Lisboa <89941040+gabrielpn06@users.noreply.github.com>
2024-09-21 13:06:05 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -2,20 +2,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import HomeScreen from './screens/HomeScreen';
-import AddProductScreen from './screens/AddProductScreen';
-import ProductDetailScreen from './screens/ProductDetailScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="AddProduct" component={AddProductScreen} />
-        <Stack.Screen name="ProductDetail" component={ProductDetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
